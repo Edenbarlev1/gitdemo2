afc934d6826a025f7f81b0111c42d7e497b63d9d
Edenbarlev1 <barleveden1@gmail.com>
2024-01-11 15:04:25 +0200

    first commit

---
+++ gitdemo2/src/test/java/edenb/gitdemo2/Gitdemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package edenb.gitdemo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitdemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
